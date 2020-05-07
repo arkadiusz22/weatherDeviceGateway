6dd1e6c57369be36ec448e1e7bee451d2e9d8833
azawadzki <azawadzki@microstrategy.com>
2020-05-07 21:31:10 +0200

    Chane interval to 20min, add helper for comunication with thingsboard

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import DataHelper from './src/dataHelper';
+import ThingsboardHelper from './src/thingsboardHelper';
 import express from 'express';
 
 const PORT = process.env.PORT || 5000
 let data = [];
+let initialFetch = true;
 
 const app = express();
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('view.ejs', { data }));
-app.get('/data', (req, res) => res.send(data));
+app.get('/', (req, res) => res.render('view.ejs', { data: DataHelper.convertTimeForFrontend(data) }));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const delay = 30 * 60 * 1000;
+const delay = 20 * 60 * 1000;
 (async function fetchData() {
     data = await DataHelper.getData();
+    if (initialFetch) {
+        ThingsboardHelper.registerAllDevices(data);
+        initialFetch = false;
+    }
+    ThingsboardHelper.sendDataToThingsboard(data);
     setTimeout(fetchData, delay);
 })();
